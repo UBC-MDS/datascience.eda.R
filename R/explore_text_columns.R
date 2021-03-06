df5dea9294b8ef848bb65ca1f1337a551a7a5706
Charles Suresh <emailcharlesjosh@gmail.com>
2021-03-07 03:38:13 +0530

    added explore_text_columns.R and test script

---
+++ R/explore_text_columns.R
@@ -0,0 +1,19 @@
+#' Performs EDA of text features
+#' - prints the summary statistics of character length
+#' - plots the distribution of character length
+#' - prints the summary statistics of word count
+#' - plots the distribution of word count
+#' - plots the word cloud
+#'
+#' @param df dataframe: dataset
+#' @param num_cols vector of numeric column names
+#' @param eps vector of the epsilon neighborhood size
+#'
+#' @return a list of plots
+#' @export
+#'
+#' @examples
+#' explore_text_columns(data_df)
+explore_text_columns <- function(df, text_cols=NULL){
+
+}
