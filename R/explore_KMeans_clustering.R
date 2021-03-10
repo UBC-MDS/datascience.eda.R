16683e5260cb6cd4f18d246e99d372e9bb51c72a
Mai Le <lephanthuymai@gmail.com>
2021-03-10 17:22:50 +0700

    complete some unit tests for explore_KMeans_clustering

--- R/explore_KMeans_clustering.R
+++ R/explore_KMeans_clustering.R
@@ -1,7 +1,6 @@
 #' fits and plots KMeans clusterings on numeric columns of the dataset
 #'
 #' @param df dataframe
-#' @param num_cols vector of numeric column names, default to NULL, all numeric columns will be used
 #' @param centers vector of number of clusters
 #' @param iter.max integer: max number of iteration
 #' @param algorithm character: may be abbreviated. Note that "Lloyd" and "Forgy" are alternative names for one algorithm
@@ -10,14 +9,56 @@
 #'
 #' @examples
 #' \dontrun{
-#' explore_KMeans_clustering(df)
+#' library(palmerpenguins)
+#' explore_KMeans_clustering(penguins)
 #' }
+
 explore_KMeans_clustering <-
-  function(df, num_cols = NULL,
-           centers,
+  function(df,
+           centers = seq(from = 2, to = 10),
            iter.max = 10,
            algorithm = c("Hartigan-Wong", "Lloyd", "Forgy",
-                         "MacQueen")
-           ) {
+                         "MacQueen"))
+  {
+    numeric_cols <-  dplyr::select_if(df, is.numeric)
 
+    # verify inputs
+    if (length(numeric_cols) == 0)
+      stop("There is no numeric columns to be clustered.")
+
+    if (!is.numeric(centers))
+      stop("Centers must be a numeric vector.")
+
+    if (is.vector(centers))
+      for (c in centers)
+        if (c<1)
+          stop("Centers must be >= 1.")
+    else
+      if (centers<1)
+        stop("Centers must be >= 1.")
+
+    if (!algorithm %in% c("Hartigan-Wong", "Lloyd", "Forgy", "MacQueen"))
+      stop("Invalid value for algorithm.")
+
+    if (!is.numeric(iter.max) | !all.equal(iter.max, as.integer(iter.max)) | iter.max<1)
+      stop("Invalid value for iter.max.")
+
+    # scale and impute the dataset before applying KMeans
+    numeric_cols <- imputeR::guess(numeric_cols)
+    numeric_cols <- scale(numeric_cols)
+
+    N = length(centers)
+    results <- vector("list", N)
+    pca = prcomp(numeric_cols, scale=FALSE)
+    for (i in seq_along(centers)) {
+      cl = kmeans(numeric_cols, centers = centers[i], algorithm = algorithm)
+      results[[i]] = (ggbiplot::ggbiplot(
+        pca,
+        groups = as.factor(cl$cluster),
+        choices = c(1, 2),
+        ellipse = TRUE,
+        var.axes = FALSE
+      ) + ggtitle(paste0("PCA Plot - KMeans with", centers[i]," centers")))
+    }
+    results
   }
--- tests/testthat/test-explore_KMeans_clustering.R
+++ tests/testthat/test-explore_KMeans_clustering.R
@@ -1,14 +1,34 @@
 test_that("KMeans - default parameters work correctly", {
-  expect_equal(2 * 2, 4)
   df <- create_test_data()
-  results <- explore_KMeans_clustering(df, centers=3)
+  results <- explore_KMeans_clustering(df)
+  expect_equal(length(results), 9)
+  centers <- seq(2, 10)
+  for (i in seq_along(centers)) {
+    p <- results[[i]]
+    expect_equal(p$labels$title, paste0("PCA Plot - KMeans with", centers[i]," centers"))
+    expect_equal(nlevels(p$data$groups), centers[i])
+  }
 })
 
 
 test_that("KMeans - invalid parameters work correctly", {
-  expect_equal(2 * 2, 4)
+  df <- create_test_data()
+
+  # there is no numeric columns
+  char_cols <- df %>% dplyr::select_if(is.character)
+  expect_error(explore_KMeans_clustering(char_cols))
+
+  # invalid number of clusters
+  expect_error(explore_KMeans_clustering(df,"a"))
+  expect_error(explore_KMeans_clustering(df,0))
+  expect_error(explore_KMeans_clustering(df,c(0, 2)))
+
+  # invalid algorithm
+  expect_error(explore_KMeans_clustering(df, algorithm = "test"))
 })
 
 test_that("KMeans - custom parameters work correctly", {
-  expect_equal(2 * 2, 4)
+  df <- create_test_data()
+  results <- explore_KMeans_clustering(df, seq(3, 5), iter.max = 15,
+                            algorithm = "Lloyd")
 })
