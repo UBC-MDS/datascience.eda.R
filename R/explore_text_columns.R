50d4d79abd8f087ceff3737cedd675f0f28801e1
Charles Suresh <emailcharlesjosh@gmail.com>
2021-03-12 07:27:22 +0530

    test and code for summ stats char length

--- R/explore_text_columns.R
+++ R/explore_text_columns.R
@@ -14,6 +14,35 @@
 #' \dontrun{
 #' explore_text_columns(df)
 #' }
-explore_text_columns <- function(df, text_cols=NULL){
+explore_text_columns <- function(df, text_cols=list()) {
 
+  for (col in text_cols){
+
+    results <- list()
+
+    cat("## Exploratory Data Analysis of \"",col,"\" column:", sep = "")
+    cat("\n")
+    cat("### Character Length:")
+    cat("\n")
+
+    char_len <- df %>%
+      dplyr::mutate(char_len = str_length(!!sym(col))) %>%
+      dplyr::pull(char_len)
+
+    mean_char_len <- round(mean(char_len), 2)
+    median_char_len <- round(median(char_len), 2)
+
+    cat("- The average character length of text is:",mean_char_len)
+    cat("\n")
+    cat("\n")
+    cat("- The median character length of text is:",median_char_len)
+    cat("\n")
+    cat("\n")
+
+    results <- append(results, mean_char_len)
+    results <- append(results, median_char_len)
+
+  }
+
+  results
 }
--- tests/testthat/test-explore_text_columns.R
+++ tests/testthat/test-explore_text_columns.R
@@ -1,3 +1,16 @@
+library(tidyverse)
+
 test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+  df <- read_csv("data/spam.csv", locale(encoding = "latin1"),
+                 col_names=c("target", "sms", "col3", "col4", "col5"),
+                 col_types=NULL)
+
+  df <- df %>% select("target", "sms")
+
+  results <- explore_text_columns(df, text_cols="sms")
+
+  expect_equal(results[[1]], 80.07)
+  expect_equal(results[[2]], 61)
+
 })
